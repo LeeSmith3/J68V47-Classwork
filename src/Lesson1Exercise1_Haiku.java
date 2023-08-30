3204e4dc9f21f26c326077fa730133cc1aa63430
LeeSmith3 <30130234@nescol.ac.uk>
2023-08-30 11:38:09 +0100

    add haiku exercise

---
+++ src/Lesson1Exercise1_Haiku.java
@@ -0,0 +1,7 @@
+public class Lesson1Exercise1_Haiku {
+    public static void main(String[] args) {
+        System.out.println("Dancing beams of light,");
+        System.out.println("Nature's canvas comes alive,");
+        System.out.println("Sunshine paints the day.");
+    }
+}
