6d78decd6c6d813aa6526184a377169179b148e4
LeeSmith3 <30130234@nescol.ac.uk>
2023-08-30 11:40:00 +0100

    Add comments to hello world class

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,3 +1,8 @@
+/**
+ * My First Program
+ * @author 30130234
+ * @version 1
+ */
 public class HelloWorld {
     public static void main (String[] args){
         System.out.println("Hello World");
