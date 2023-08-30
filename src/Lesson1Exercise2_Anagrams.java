0a9fed49a994dc0f83a5e253a6e53cb4a824d81a
LeeSmith3 <30130234@nescol.ac.uk>
2023-08-30 11:54:48 +0100

    Add anagrams class

---
+++ src/Lesson1Exercise2_Anagrams.java
@@ -0,0 +1,10 @@
+public class Lesson1Exercise2_Anagrams {
+    public static void main (String[] args){
+        System.out.println("Here are 5 9-letter anagrams:");
+        System.out.println("A done band = Abandoned");
+        System.out.println("Benji brag = Jabbering");
+        System.out.println("make recap = Pacemaker");
+        System.out.println("Rack trace = Racetrack");
+        System.out.println("Badge oats = Sabotaged");
+    }
+}
