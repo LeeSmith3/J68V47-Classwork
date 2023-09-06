56d12e1ba17f3e8d245c509668fc492862daf21e
LeeSmith3 <30130234@nescol.ac.uk>
2023-09-06 11:41:32 +0100

    .

--- src/Lesson2Exercise3_Formatting.java
+++ src/Lesson2Exercise3_Formatting.java
@@ -5,19 +5,19 @@ public class Lesson2Exercise3_Formatting {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the name of the first item: ");
-        int name = input.nextInt();
+        String name = input.next();
 
         System.out.print("Enter the price of the first item: ");
         double firstItem = input.nextDouble();
 
         System.out.print("Enter the name of the second item: ");
-        int name2 = input.nextInt();
+        String name2 = input.next();
 
         System.out.print("Enter the price of the second item: ");
         double secondItem = input.nextDouble();
 
         System.out.print("Enter the name of the third item: ");
-        int name3 = input.nextInt();
+        String name3 = input.next();
 
         System.out.print("Enter the price of the third item: ");
         double thirdItem = input.nextDouble();
