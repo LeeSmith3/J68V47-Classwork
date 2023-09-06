b4e9c52a7996c350b3a7d5dd9b55d97ad4dac37c
LeeSmith3 <30130234@nescol.ac.uk>
2023-09-06 09:28:17 +0100

    Add input code

--- src/Lesson2Exercise1_ShortStory.java
+++ src/Lesson2Exercise1_ShortStory.java
@@ -1,2 +1,13 @@
+
+import java.util.*;
 public class Lesson2Exercise1_ShortStory {
+    public static void main (String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("What is your name? ");
+        String name = input.nextLine();
+
+        System.out.print("What is a hobby of yours? ");
+        String hobby = input.nextLine();
+    }
+
 }
