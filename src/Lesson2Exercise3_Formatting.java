140ec08ec60f9bff4700e05c3d721470f05d7a05
LeeSmith3 <30130234@nescol.ac.uk>
2023-09-06 11:36:20 +0100

    .

--- src/Lesson2Exercise3_Formatting.java
+++ src/Lesson2Exercise3_Formatting.java
@@ -4,20 +4,29 @@ public class Lesson2Exercise3_Formatting {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.print("Enter the name of the first item: ");
+        int name = input.nextInt();
+
         System.out.print("Enter the price of the first item: ");
         double firstItem = input.nextDouble();
 
+        System.out.print("Enter the name of the second item: ");
+        int name2 = input.nextInt();
+
         System.out.print("Enter the price of the second item: ");
         double secondItem = input.nextDouble();
+
+        System.out.print("Enter the name of the third item: ");
+        int name3 = input.nextInt();
 
         System.out.print("Enter the price of the third item: ");
         double thirdItem = input.nextDouble();
 
         double total = firstItem + secondItem + thirdItem;
 
-        System.out.format("\nITEM 1........ £%.2f", firstItem);
-        System.out.format("\nITEM 2........ £%.2f", secondItem);
-        System.out.format("\nITEM 3........ £%.2f", thirdItem);
+        System.out.format("\n"+ name +"........ £%.2f", firstItem);
+        System.out.format("\n"+ name2 +"........ £%.2f", secondItem);
+        System.out.format("\n"+ name3 +"........ £%.2f", thirdItem);
         System.out.format("\nSUBTOTAL....... £%.2f", total);
     }
 
