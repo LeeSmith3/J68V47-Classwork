34b4b612c6ae3eee6f6521a17dfc8614b25dbd49
LeeSmith3 <30130234@nescol.ac.uk>
2023-09-06 09:52:31 +0100

    Add output code

--- src/Lesson2Exercise1_ShortStory.java
+++ src/Lesson2Exercise1_ShortStory.java
@@ -6,8 +6,10 @@ public class Lesson2Exercise1_ShortStory {
         System.out.print("What is your name? ");
         String name = input.nextLine();
 
-        System.out.print("What is a hobby of yours? ");
+        System.out.print("What is a hobby of yours i.e. programming, bowling, playing basketball? ");
         String hobby = input.nextLine();
+
+        System.out.print("Today, " + name + " had a good day! They spent their day " + hobby + " and enjoyed themselves very much. \nAfter a long day of " + hobby + ", " + name + " was very tired, and decided to head to bed, feeling satisfied with their day. \n The end.");
     }
 
 }
