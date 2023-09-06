69dff46889829fd44af55924cb33a8a89941a8b5
LeeSmith3 <30130234@nescol.ac.uk>
2023-09-06 09:55:00 +0100

    Add relevant comments

--- src/Lesson2Exercise1_ShortStory.java
+++ src/Lesson2Exercise1_ShortStory.java
@@ -2,6 +2,7 @@
 import java.util.*;
 public class Lesson2Exercise1_ShortStory {
     public static void main (String[] args){
+        /* Collects user input */
         Scanner input = new Scanner(System.in);
         System.out.print("What is your name? ");
         String name = input.nextLine();
@@ -9,6 +10,7 @@ public class Lesson2Exercise1_ShortStory {
         System.out.print("What is a hobby of yours i.e. programming, bowling, playing basketball? ");
         String hobby = input.nextLine();
 
+        /* Displays short story output w/ concatenation */
         System.out.print("Today, " + name + " had a good day! They spent their day " + hobby + " and enjoyed themselves very much. \nAfter a long day of " + hobby + ", " + name + " was very tired, and decided to head to bed, feeling satisfied with their day. \n The end.");
     }
 
