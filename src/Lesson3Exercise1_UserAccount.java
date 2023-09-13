e6dbf1bf5708f8f479b3318f8a948b7a74316c94
LeeSmith3 <30130234@nescol.ac.uk>
2023-09-13 09:53:16 +0100

    add input code

--- src/Lesson3Exercise1_UserAccount.java
+++ src/Lesson3Exercise1_UserAccount.java
@@ -1,2 +1,17 @@
+
+import java.util.*;
 public class Lesson3Exercise1_UserAccount {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your first name: ");
+        String firstName = input.next();
+
+        System.out.print("Enter your surname: ");
+        String surname = input.next();
+
+        System.out.print("Enter your year of birth: ");
+        String yrOfBirth = input.next();
+    }
+
 }
