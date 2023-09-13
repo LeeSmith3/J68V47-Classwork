f55675ab8d65f09de1dc294d07f97c0bf51a9e4d
LeeSmith3 <30130234@nescol.ac.uk>
2023-09-13 12:01:11 +0100

    Add code

--- src/Lesson3Exercise3_Loan.java
+++ src/Lesson3Exercise3_Loan.java
@@ -1,2 +1,23 @@
+import java.util.*;
 public class Lesson3Exercise3_Loan {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Loan amount: £");
+        int loan = input.nextInt();
+
+        System.out.println("Interest Rate (APR %): ");
+        int interest = input.nextInt();
+
+        System.out.println("Number of years: ");
+        int years = input.nextInt();
+
+        double j = ((double)interest / 100) / 12;
+        double n = years * 12;
+        double Monthly = loan * (j / (1 - Math.pow((1+ j),-n)));
+
+        System.out.format("Monthly payment for this loan: £%.2f", Monthly);
+
+
+    }
 }
