4c7654904705eeab8ffde62cd6e1e428536dd266
LeeSmith3 <30130234@nescol.ac.uk>
2023-09-20 11:23:56 +0100

    L4E2

--- src/Lesson3Exercise2_Quiz.java
+++ src/Lesson3Exercise2_Quiz.java
@@ -7,17 +7,30 @@ public class Lesson3Exercise2_Quiz {
         String capitalSpain = input.next();
         capitalSpain = capitalSpain.toLowerCase();
 
+        if (!capitalSpain.equals("madrid")){
+            System.out.println("Sorry, the correct answer is Madrid.");
+        }
+
+
         System.out.println(capitalSpain.contains("madrid"));
 
         System.out.print("Q2. What is the capital of the UK? ");
         String capitalUK = input.next();
         capitalUK = capitalUK.toLowerCase();
 
+        if (!capitalUK.equals("london")){
+            System.out.println("Sorry, the correct answer is London.");
+        }
+
         System.out.println(capitalUK.contains("london"));
 
         System.out.print("Q3. What is the capital of Italy? ");
         String capitalItaly = input.next();
         capitalItaly = capitalItaly.toLowerCase();
+
+        if (!capitalItaly.equals("rome")){
+            System.out.println("Sorry, the correct answer is Rome.");
+        }
 
         System.out.println(capitalItaly.contains("rome"));
 
