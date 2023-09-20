19a10e7e2469f1bdece411280275a8a3b40dacff
LeeSmith3 <30130234@nescol.ac.uk>
2023-09-20 11:58:40 +0100

    L4E3

--- src/Lesson4Exercise3_Shipping.java
+++ src/Lesson4Exercise3_Shipping.java
@@ -1,6 +1,18 @@
 import java.util.*;
 public class Lesson4Exercise3_Shipping {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your total purchase amount: £");
+        Float purchaseAmount = input.nextFloat();
+
+        if (purchaseAmount < 50) {
+            System.out.print("Shipping cost is = £10.00");
+            System.out.format("\nYour final total is = £%.2f", purchaseAmount + 10);
+        } else {
+            System.out.format("\nYour final total is = £%.2f", purchaseAmount);
+        }
+
 
     }
 }
