3aa7adba1c20b0d9d19822325f0021511037bd07
LeeSmith3 <30130234@nescol.ac.uk>
2023-09-20 10:01:18 +0100

    Create inputs and validation

--- src/Lesson4Exercise1_Deposit.java
+++ src/Lesson4Exercise1_Deposit.java
@@ -1,2 +1,29 @@
+import java.util.*;
 public class Lesson4Exercise1_Deposit {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your first name: ");
+        String firstName = input.next();
+
+        System.out.println("Enter your surname: ");
+        String surname = input.next();
+
+        System.out.println("Enter the total value of your order: ");
+        float orderVal = input.nextFloat();
+
+        System.out.println("Enter the amount you wish to pay as deposit: ");
+        float deposit = input.nextFloat();
+
+        if (deposit > orderVal) {
+            System.out.println("Deposit amount too large. Please try again.");
+            deposit = input.nextFloat();
+        } else if (deposit < 0) {
+            System.out.println("Deposit cannot be a negative number. Please try again");
+            deposit = input.nextFloat();
+        }
+
+
+
+    }
 }
