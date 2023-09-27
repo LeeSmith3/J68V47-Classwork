cf2823c8ad08097c182d63acd6e713afb8bdd1b6
LeeSmith3 <30130234@nescol.ac.uk>
2023-09-27 11:05:37 +0100

    L5E2

--- src/Lesson5Exercise2_blastOff.java
+++ src/Lesson5Exercise2_blastOff.java
@@ -1,6 +1,6 @@
 public class Lesson5Exercise2_blastOff {
     public static void main(String[] args) {
-        int countdownStart = 10;
+        int countdownStart = 11;
         for( int count=1; count<=10; count++ )
         {
             System.out.println(countdownStart - count);
