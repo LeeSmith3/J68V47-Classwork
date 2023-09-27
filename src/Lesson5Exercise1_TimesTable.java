4316c56ee1d3bd5351bb4a81ea9faf1d7cfecaa3
LeeSmith3 <30130234@nescol.ac.uk>
2023-09-27 09:52:15 +0100

    L5E1

--- src/Lesson5Exercise1_TimesTable.java
+++ src/Lesson5Exercise1_TimesTable.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
 public class Lesson5Exercise1_TimesTable {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a base number: ");
+        int num = input.nextInt();
 
+        for( int count=1; count<=12; count++ )
+        {
+            System.out.println(num+" multiplied by " +count+ " is equal to "+ num * count);
+        }
     }
 }
