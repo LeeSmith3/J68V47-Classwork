8d9eb4a447f7241885700d8f3a5aa6fbcbba4c4f
LeeSmith3 <30130234@nescol.ac.uk>
2023-10-04 10:33:06 +0100

    factorial calculator extra

--- src/Lesson5A_factorialcalc.java
+++ src/Lesson5A_factorialcalc.java
@@ -1,2 +1,18 @@
+import java.util.*;
+
 public class Lesson5A_factorialcalc {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your number here: ");
+        int userNum = input.nextInt();
+
+        System.out.print("Enter the amount of times you want to multiply the number here: ");
+        int multiplyBy = input.nextInt();
+        for(int i = 1; i <= multiplyBy; i++ ){
+            int number = userNum * i;
+            System.out.println(userNum+" x "+i+" = "+number);
+        }
+    }
 }
