5ff81a20a375f189f02629504b9c3b9db925a53c
LeeSmith3 <30130234@nescol.ac.uk>
2023-10-04 10:37:07 +0100

    L6E1

---
+++ src/Lesson6Exercise1_EvenNumbers.java
@@ -0,0 +1,10 @@
+public class Lesson6Exercise1_EvenNumbers {
+    public static void main(String[] args) {
+        int startingNumber = 10;
+        System.out.println(startingNumber);
+        while(startingNumber < 24){
+            startingNumber = startingNumber + 2;
+            System.out.println(startingNumber);
+        }
+    }
+}
