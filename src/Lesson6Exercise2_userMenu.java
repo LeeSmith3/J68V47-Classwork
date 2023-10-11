da15f7234865d8c07b36703040760840ce781050
LeeSmith3 <30130234@nescol.ac.uk>
2023-10-11 10:14:16 +0100

    .

--- src/Lesson6Exercise2_userMenu.java
+++ src/Lesson6Exercise2_userMenu.java
@@ -9,9 +9,9 @@ public class Lesson6Exercise2_userMenu {
         while(!isQuitting){
 
             System.out.println("Please select one of the following options:\n1. Say 'hello'\n2. Tell a joke\n3. Quit.");
-            var userInput = input.nextLine();
+            var option = input.nextLine();
 
-            if(userInput.equals("3")){
+            if(option.equals("3")){
                 isQuitting = true;
                 System.out.println("Goodbye.");
             }
