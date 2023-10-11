29fc60bd0555b226f883395c61b29a16c7968554
LeeSmith3 <30130234@nescol.ac.uk>
2023-10-11 12:03:14 +0100

    L7E2

---
+++ src/Lesson7Exercise2_SumFunction.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Lesson7Exercise2_SumFunction {
+
+    public static int valueA(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the first number: ");
+        int valueA = input.nextInt();
+        return valueA;
+    }
+    public static int valueB(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the second number: ");
+        int valueB = input.nextInt();
+        return valueB;
+    }
+    public static int Sum;
+    public static void printAnswer(){
+        System.out.print("The sum of "+valueA() +" and "+valueB() +" is "+calculateSum());
+    }
+
+    public static int calculateSum(){
+        int valueA = valueA();
+        int valueB = valueB();
+        int calculateSum = valueA + valueB;
+        return calculateSum;
+    }
+    public static void main(String[] args) {
+        printAnswer();
+    }
+}
