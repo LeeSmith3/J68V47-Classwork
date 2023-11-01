0a8a4a648a6f1efedd7ddf80fd8634522992b6f2
LeeSmith3 <30130234@nescol.ac.uk>
2023-11-01 11:54:02 +0000

    L8E2

---
+++ src/Lesson8Exercise2_ExamScores.java
@@ -0,0 +1,8 @@
+public class Lesson8Exercise2_ExamScores {
+    public static void main(String[] args) {
+        int examScores[] = {23,47,63,99,55,43,12,78,67,88};
+        for(int index = 0; index < examScores.length; index++){
+            System.out.println(examScores[index]);
+        }
+    }
+}
